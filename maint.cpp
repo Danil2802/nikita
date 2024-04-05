04de7603a730fd8620baa159c9d91117a7992f6e
Assweg <162096346+Assweg@users.noreply.github.com>
2024-04-05 13:13:07 +0700

    Update maint.cpp

--- maint.cpp
+++ maint.cpp
@@ -3,47 +3,43 @@
 
 using namespace std;
 
-class Trapezium {
-private:
-    double a, b, h, c;
+// Функция для вычисления периметра трапеции
+float calculateTrapezoidPerimeter(float side1, float side2, float base1, float base2) {
+    return side1 + side2 + base1 + base2;
+}
 
-public:
-    Trapezium(double topBase, double bottomBase, double height, double side) {
-        a = topBase;
-        b = bottomBase;
-        h = height;
-        c = side;
-    }
+// Функция для вычисления площади трапеции
+float calculateTrapezoidArea(float base1, float base2, float height) {
+    return (base1 + base2) * height / 2;
+}
 
-    double getPerimeter() {
-        return a + b + 2 * c;
-    }
-
-    double getArea() {
-        return 0.5 * (a + b) * h;
-    }
-
-    double getMidLineLength() {
-        return (a + b) / 2;
-    }
-};
+// Функция для вычисления длины средней линии трапеции
+float calculateTrapezoidMedian(float base1, float base2) {
+    return (base1 + base2) / 2;
+}
 
 int main() {
-    double topBase, bottomBase, height, side;
-    cout << "Введите верхнюю основу трапеции: ";
-    cin >> topBase;
-    cout << "Введите нижнюю основу трапеции: ";
-    cin >> bottomBase;
+    float side1, side2, base1, base2, height;
+    cout << "Введите длину первого бокового ребра трапеции: ";
+    cin >> side1;
+    cout << "Введите длину второго бокового ребра трапеции: ";
+    cin >> side2;
+    cout << "Введите длину верхнего основания трапеции: ";
+    cin >> base1;
+    cout << "Введите длину нижнего основания трапеции: ";
+    cin >> base2;
     cout << "Введите высоту трапеции: ";
     cin >> height;
-    cout << "Введите длину боковой стороны: ";
-    cin >> side;
 
-    Trapezium trapezium(topBase, bottomBase, height, side);
+    // Проверка на отрицательные числа
+    if (side1 < 0 || side2 < 0 || base1 < 0 || base2 < 0 || height < 0) {
+        cout << "Введите положительные числа!" << endl;
+        return 1;
+    }
 
-    cout << "Периметр трапеции: " << trapezium.getPerimeter() << std::endl;
-    cout << "Площадь трапеции: " << trapezium.getArea() << std::endl;
-    cout << "Длина средней линии трапеции: " << trapezium.getMidLineLength() << std::endl;
+    cout << "Периметр трапеции: " << calculateTrapezoidPerimeter(side1, side2, base1, base2) << endl;
+    cout << "Площадь трапеции: " << calculateTrapezoidArea(base1, base2, height) << endl;
+    cout << "Длина средней линии трапеции: " << calculateTrapezoidMedian(base1, base2) << endl;
 
     return 0;
 }
